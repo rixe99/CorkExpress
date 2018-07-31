f22ec1309ffa7e7d370b53332795fe0a6f4777f6
rixe99 <g.lavrador10@gmail.com>
2018-07-31 23:48:43 +0100

    more change on an1

--- CORKExpress/BackOffice/admin/an1.php
+++ CORKExpress/BackOffice/admin/an1.php
@@ -67,6 +67,22 @@
                         </select>
                     </div>
                   </div>
+                  <div class="row form-group">
+                        <div class="col col-md-3">
+                            <label for="text-input" class=" form-control-label">Username:</label>
+                        </div>
+                        <div class="col-12 col-md-9">
+                            <input type="text" id="text-input" name="username" placeholder="Username" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                          <div class="col col-md-3">
+                              <label for="text-input" class=" form-control-label">Password:</label>
+                          </div>
+                          <div class="col-12 col-md-9">
+                              <input type="text" id="text-input" name="password" placeholder="Password" class="form-control">
+                          </div>
+                      </div>
           <div class="card-footer">
               <button type="submit" class="btn btn-primary btn-sm">
                   <i class="fa fa-dot-circle-o"></i> Submit
