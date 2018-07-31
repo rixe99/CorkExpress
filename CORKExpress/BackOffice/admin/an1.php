517eee227950e4fde448dad222373cf142232349
rixe99 <g.lavrador10@gmail.com>
2018-07-31 22:55:09 +0100

    foi adicionado e alterados nos inputs do formulario

--- CORKExpress/BackOffice/admin/an1.php
+++ CORKExpress/BackOffice/admin/an1.php
@@ -11,7 +11,7 @@
                       <label for="text-input" class=" form-control-label">Nome:</label>
                   </div>
                   <div class="col-12 col-md-9">
-                      <input type="text" id="text-input" name="nome" placeholder="Text" class="form-control">
+                      <input type="text" id="text-input" name="nome" placeholder="Nome" class="form-control">
                   </div>
               </div>
               <div class="row form-group">
@@ -19,9 +19,17 @@
                         <label for="text-input" class=" form-control-label">Apelido:</label>
                     </div>
                     <div class="col-12 col-md-9">
-                        <input type="text" id="text-input" name="apelido" placeholder="Text" class="form-control">
+                        <input type="text" id="text-input" name="apelido" placeholder="Apelido" class="form-control">
                     </div>
                 </div>
+                <div class="row form-group">
+                      <div class="col col-md-3">
+                          <label for="text-input" class=" form-control-label">Morada:</label>
+                      </div>
+                      <div class="col-12 col-md-9">
+                          <input type="text" id="text-input" name="morada" placeholder="Morada" class="form-control">
+                      </div>
+                  </div>
                 <div class="row form-group">
                     <div class="col col-md-3">
                         <label for="email-input" class=" form-control-label">Email:</label>
@@ -32,20 +40,33 @@
                 </div>
                 <div class="row form-group">
                       <div class="col col-md-3">
-                          <label for="text-input" class=" form-control-label">Apelido:</label>
+                          <label for="text-input" class=" form-control-label">Niss:</label>
                       </div>
                       <div class="col-12 col-md-9">
-                          <input type="text" id="text-input" name="apelido" placeholder="Text" class="form-control">
+                          <input type="number" id="text-input" name="niss" placeholder="Niss" class="form-control">
                       </div>
                   </div>
                   <div class="row form-group">
                         <div class="col col-md-3">
-                            <label for="text-input" class=" form-control-label">Apelido:</label>
+                            <label for="text-input" class=" form-control-label">Nib:</label>
                         </div>
                         <div class="col-12 col-md-9">
-                            <input type="text" id="text-input" name="apelido" placeholder="Text" class="form-control">
+                            <input type="number" id="text-input" name="nib" placeholder="Nib" class="form-control">
                         </div>
                     </div>
+                  <div class="row form-group">
+                      <div class="col col-md-3">
+                        <label for="select" class=" form-control-label">Categoria do trabalhador:</label>
+                      </div>
+                    <div class="col-12 col-md-9">
+                        <select name="select" id="select" class="form-control">
+                            <option value="0">Please select</option>
+                            <option value="1">Option #1</option>
+                            <option value="2">Option #2</option>
+                            <option value="3">Option #3</option>
+                        </select>
+                    </div>
+                  </div>
           <div class="card-footer">
               <button type="submit" class="btn btn-primary btn-sm">
                   <i class="fa fa-dot-circle-o"></i> Submit
