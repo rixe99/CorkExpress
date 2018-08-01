efa108c68a9f4f086ab43b056805f78d555e9665
rixe99 <g.lavrador10@gmail.com>
2018-08-01 20:17:00 +0100

    foi adicionado ensirir e listar de admins algumas alterações no user

---
+++ CORKExpress/BackOffice/admin/an3.php
@@ -0,0 +1,73 @@
+<center>
+    <div class="col-lg-6">
+      <div class="card">
+          <div class="card-header">
+              <strong>Inserir dados  do </strong> Administrador
+          </div>
+          <div class="card-body card-block">
+          <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+              <div class="row form-group">
+                  <div class="col col-md-3">
+                      <label for="text-input" class=" form-control-label">Nome:</label>
+                  </div>
+                  <div class="col-12 col-md-9">
+                      <input type="text" id="text-input" name="nome" placeholder="Nome" class="form-control">
+                  </div>
+              </div>
+              <div class="row form-group">
+                    <div class="col col-md-3">
+                        <label for="text-input" class=" form-control-label">Apelido:</label>
+                    </div>
+                    <div class="col-12 col-md-9">
+                        <input type="text" id="text-input" name="apelido" placeholder="Apelido" class="form-control">
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col col-md-3">
+                        <label for="email-input" class=" form-control-label">Email:</label>
+                    </div>
+                    <div class="col-12 col-md-9">
+                        <input type="email" id="email-input" name="email" placeholder="Enter Email" class="form-control">
+                    </div>
+                </div>
+                  <div class="row form-group">
+                      <div class="col col-md-3">
+                        <label for="select" class=" form-control-label">Categoria do trabalhador:</label>
+                      </div>
+                    <div class="col-12 col-md-9">
+                        <select name="select" id="select" class="form-control">
+                            <option value="0">Please select</option>
+                            <option value="1">Option #1</option>
+                            <option value="2">Option #2</option>
+                            <option value="3">Option #3</option>
+                        </select>
+                    </div>
+                  </div>
+                  <div class="row form-group">
+                        <div class="col col-md-3">
+                            <label for="text-input" class=" form-control-label">Username:</label>
+                        </div>
+                        <div class="col-12 col-md-9">
+                            <input type="text" id="text-input" name="username" placeholder="Username" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                          <div class="col col-md-3">
+                              <label for="text-input" class=" form-control-label">Password:</label>
+                          </div>
+                          <div class="col-12 col-md-9">
+                              <input type="text" id="text-input" name="password" placeholder="Password" class="form-control">
+                          </div>
+                      </div>
+          <div class="card-footer">
+              <button type="submit" class="btn btn-primary btn-sm">
+                  <i class="fa fa-dot-circle-o"></i> Submit
+              </button>
+              <button type="reset" class="btn btn-danger btn-sm">
+                  <i class="fa fa-ban"></i> Reset
+              </button>
+          </div>
+        </form>
+      </div>
+  </div>
+<center>
---
+++ CORKExpress/BackOffice/admin/an4.php
@@ -0,0 +1,30 @@
+<center>
+  <div class="row">
+    <div class="col-lg-9" style="max-width:100%;">
+        <div class="table-responsive table--no-card m-b-30">
+            <table class="table table-borderless table-striped table-earning">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Apelido</th>
+                        <th>Email</th>
+                        <th class="text-right">Categoria</th>
+                        <th class="text-right">Username</th>
+                        <th class="text-right">Password</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2018-09-29 05:57</td>
+                        <td>100398</td>
+                        <td>$999.00</td>
+                        <td class="text-right">1</td>
+                        <td class="text-right">$999.00</td>
+                        <td class="text-right">$999.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</center>
