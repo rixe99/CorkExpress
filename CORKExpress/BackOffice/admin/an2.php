5e914590d8d1135f8de233ac6615a96c731c48ec
rixe99 <g.lavrador10@gmail.com>
2018-08-01 19:06:44 +0100

    more change

--- CORKExpress/BackOffice/admin/an2.php
+++ CORKExpress/BackOffice/admin/an2.php
@@ -1,37 +1,36 @@
-
-<center>
-    <div class="row">
-        <div class="col-lg-9">
-            <div class="table-responsive table--no-card m-b-30">
-                <table class="table table-borderless table-striped table-earning">
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Apelido</th>
-                            <th>Morada</th>
-                            <th>Email</th>
-                            <th>Categoria</th>
-                            <th class="text-right">NiB</th>
-                            <th class="text-right">Niss</th>
-                            <th class="text-right">Username</th>
-                            <th class="text-right">Password</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>2018-09-29 05:57</td>
-                            <td>100398</td>
-                            <td>iPhone X 64Gb Grey</td>
-                            <td>$999.00</td>
-                            <td>1</td>
-                            <td class="text-right">$999.00</td>
-                            <td class="text-right">$999.00</td>
-                            <td class="text-right">$999.00</td>
-                            <td class="text-right">$999.00</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+<div class="row">
+  <center>
+    <div class="col-lg-9" style="width:100%;">
+        <div class="table-responsive table--no-card m-b-30">
+            <table class="table table-borderless table-striped table-earning">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Apelido</th>
+                        <th>Morada</th>
+                        <th>Email</th>
+                        <th>Categoria</th>
+                        <th class="text-right">NiB</th>
+                        <th class="text-right">Niss</th>
+                        <th class="text-right">Username</th>
+                        <th class="text-right">Password</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2018-09-29 05:57</td>
+                        <td>100398</td>
+                        <td>iPhone X 64Gb Grey</td>
+                        <td>$999.00</td>
+                        <td>1</td>
+                        <td class="text-right">$999.00</td>
+                        <td class="text-right">$999.00</td>
+                        <td class="text-right">$999.00</td>
+                        <td class="text-right">$999.00</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
-</center>
+  </center>
+</div>
