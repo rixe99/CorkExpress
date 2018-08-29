263eaab71e0f384ae6e57c43fa5b8d1e3d9bf7ee
Deusin86 <hencarnacao@sapo.pt>
2018-08-29 17:41:29 +0100

    user ver dados

---
+++ CORKExpress/BackOffice/user/an1.php
@@ -0,0 +1,47 @@
+<center>
+  <div class="row">
+    <div class="col-lg-9" style="max-width:100%;">
+        <div class="table-responsive table--no-card m-b-30">
+            <table class="table table-borderless table-striped table-earning">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Apelido</th>
+                        <th>Morada</th>
+                        <th>Email</th>
+                        <th>Categoria</th>
+                        <th class="text-right">NiB</th>
+                        <th class="text-right">NiF</th>
+                        <th class="text-right">Niss</th>
+                        <th class="text-right">Username</th>
+                        <th class="text-right">Password</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  <?php
+                      include '../../connect/conn.php';
+                      $dados = mysqli_query($conn,"SELECT idtrabalhador, nome, apelido, nif, nib, niss ,categoria, morada, email, password, username  FROM trabalhadores WHERE idtrabalhador=$_SESSION[idtrabalhador] ");
+
+                        while ($row=mysqli_fetch_assoc($dados)){
+                          echo '<tr>';
+                          echo '<td style="padding: 12px 20px;">'. $row['nome']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['apelido']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['morada']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['email']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['categoria']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['nib']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['nif']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['niss']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['username']. '</td>';
+                          echo '<td style="padding: 12px 20px;">'. $row['password']. '</td>';
+                          echo '<td style="padding: 12px 20px;"><a href="admin.php?editar='.$row["idtrabalhador"].'&an=6"><button>Editar</button></a></td>';
+                          echo '</tr>';
+                        }
+
+                  ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</center>
--- CORKExpress/BackOffice/user/user.php
+++ CORKExpress/BackOffice/user/user.php
@@ -80,6 +80,11 @@ validar();
                       <li class="has-sub">
                           <a class="js-arrow" href="#">
                               <i class="fas fa-tachometer-alt"></i>Admin</a>
+                              <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="?an=1">Dados</a>
+                                </li>
+                            </ul>
                       </li>
                         <li>
                             <a href="?an=5"><i class="fas fa-euro"></i>Salario</a>
