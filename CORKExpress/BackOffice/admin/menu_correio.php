f4405b025b303fd000c0586e75b07393c52e790b
rixe99 <g.lavrador10@gmail.com>
2018-09-07 14:16:23 +0100

    update

--- CORKExpress/BackOffice/admin/menu_correio.php
+++ CORKExpress/BackOffice/admin/menu_correio.php
@@ -9,12 +9,12 @@
   }
        while ($rows=mysqli_fetch_assoc($dados)){
          $output .= '
-           <tr>
-           <td style="width:150px">'. $rows['nome']. '</td>
-           <td style="width:150px">'. $rows['apelido']. '</td>
-           <td style="width:150px">'. $rows['email']. '</td>
-           <td style="width:150px">'. $rows['categoria']. '</td>
-           </tr>';
+         <tr>
+           <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
+           <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
+           <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
+           <td class="mailbox-date">11 hours ago</td>
+         </tr>';
        }
        echo $output;
  ?>
