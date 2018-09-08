d34ccdfb211cfcd2af21902fd3bd92afe7962b16
rixe99 <g.lavrador10@gmail.com>
2018-09-08 19:24:20 +0100

    mail

--- CORKExpress/BackOffice/admin/menu_correio.php
+++ CORKExpress/BackOffice/admin/menu_correio.php
@@ -2,18 +2,20 @@
   include '../../connect/conn.php';
   $tipo = $_POST['tipo'];
   $output = '';
-  if($tipo == 1){
-      $dados =mysqli_query($conn,"SELECT nome, apelido, categoria, email FROM trabalhadores WHERE tipouser = $tipo ORDER BY nome");
-  }else {
-    $dados =mysqli_query($conn,"SELECT nome, apelido, categoria, email FROM trabalhadores WHERE tipouser = $tipo ORDER BY nome");
+  if ($tipo == 1){
+    $dados =mysqli_query($conn,"SELECT notificacoes.idnotifics, trabalhadores.nome, trabalhadores.apelido, notificacoes.texto, notificacoes.assunto  FROM trabalhadores inner JOIN notificacoes ON trabalhadores.email = notificacoes.emaildestinatario WHERE notificacoes.emaildestinatario = 'hencanacao@pp.oo' ");
+  }
+  if ($tipo == 0){
+    $dados =mysqli_query($conn,"SELECT notificacoes.idnotifics, trabalhadores.nome, trabalhadores.apelido, notificacoes.texto, notificacoes.assunto  FROM trabalhadores inner JOIN notificacoes ON trabalhadores.email = notificacoes.emaildestinatario WHERE notificacoes.emailremetente = 'hencanacao@pp.oo' ");
   }
        while ($rows=mysqli_fetch_assoc($dados)){
+         $sub=substr($rows['texto'], 0, 50);
          $output .= '
          <tr>
-           <td class="mailbox-star"><a href=""><i class="fa fa-trash-o"></i></a></td>
-           <td class="mailbox-name"><a href="">'.$dados['nome'].'  '.$dados['apelido'].'</a></td>
-           <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-           <td class="mailbox-date">11 hours ago</td>
+           <td class="mailbox-star"><a href="#"><i class="fa fa-trash-o text-yellow"></i></a></td>
+           <td class="mailbox-name"><a href="#" onclick="entrar('.$rows['idnotifics'].')">&nbsp;'.$rows['nome'].'&nbsp;'.$rows['apelido'].'</a></td>
+           <td class="mailbox-subject"><b>&nbsp;'.$rows['assunto'].'</b> - '.$sub.'...</td>
+           <td class="mailbox-date">11&nbsp;hours&nbsp;ago</td>
          </tr>';
        }
        echo $output;
--- CORKExpress/BackOffice/admin/menu_mail.php
+++ CORKExpress/BackOffice/admin/menu_mail.php
@@ -23,6 +23,8 @@ $(document).ready(function(){
   $("#5001").hide();
   $("#5002").hide();
   $("#5003").hide();
+  $("#5005").hide();
+
 
   $("#5400").click(function(){
       $("#5437").removeClass("active");
@@ -40,6 +42,41 @@ $(document).ready(function(){
         $("#5000").hide();
         $("#5001").show();
         $("#5002").hide();
+        $tipo = 1;
+        $.ajax({
+            url:"menu_correio.php",
+            method:"POST",
+            data: {tipo: $tipo},
+            success:function(data){
+              $('#putwhere').html(data);
+              $( "#pagina" ).remove();
+              $( "#pagina" ).remove();
+              $( "#pagina" ).remove();
+              $( "#pagina" ).remove();
+              $('table.paginated').each(function() {
+                  var currentPage = 0;
+                  var numPerPage = 10;
+                  var $table = $(this);
+                  $table.bind('repaginate', function() {
+                      $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+                  });
+                  $table.trigger('repaginate');
+                  var numRows = $table.find('tbody tr').length;
+                  var numPages = Math.ceil(numRows / numPerPage);
+                  var $pager = $('<div class="pager"></div>');
+                  for (var page = 0; page < numPages; page++) {
+                      $('<button id="pagina" class="btn btn-default btn-sm"></button>').text(page + 1).bind('click', {
+                          newPage: page
+                      }, function(event) {
+                          currentPage = event.data['newPage'];
+                          $table.trigger('repaginate');
+                          $(this).addClass('active').siblings().removeClass('active');
+                      }).appendTo($pager).addClass('clickable');
+                  }
+                  $pager.insertBefore("#btscryp").find('button.btn btn-default btn-sm:first').addClass('active');
+              });
+            }
+        });
     });
     $("#5437").click(function(){
         $("#5436").removeClass("active");
@@ -165,103 +202,8 @@ $(document).ready(function(){
               <div class="box-body no-padding">
                 <div class="table-responsive mailbox-messages">
                   <table id ="myTable" class="table table-hover table-striped paginated">
-                    <tbody>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
-                    <tr>
-                      <td class="mailbox-star"><a href=""><i class="fa fa-star text-yellow"></i></a></td>
-                      <td class="mailbox-name"><a href="">Alexander Pierce</a></td>
-                      <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...</td>
-                      <td class="mailbox-date">11 hours ago</td>
-                    </tr>
+                    <tbody id="putwhere">
+
                     </tbody>
                   </table>
                 </div>
@@ -272,30 +214,6 @@ $(document).ready(function(){
       </div>
       </section>
       </div>
-    <script >
-    $('table.paginated').each(function() {
-        var currentPage = 0;
-        var numPerPage = 10;
-        var $table = $(this);
-        $table.bind('repaginate', function() {
-            $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-        });
-        $table.trigger('repaginate');
-        var numRows = $table.find('tbody tr').length;
-        var numPages = Math.ceil(numRows / numPerPage);
-        var $pager = $('<div class="pager"></div>');
-        for (var page = 0; page < numPages; page++) {
-            $('<button class="btn btn-default btn-sm"></button>').text(page + 1).bind('click', {
-                newPage: page
-            }, function(event) {
-                currentPage = event.data['newPage'];
-                $table.trigger('repaginate');
-                $(this).addClass('active').siblings().removeClass('active');
-            }).appendTo($pager).addClass('clickable');
-        }
-        $pager.insertBefore("#btscryp").find('button.btn btn-default btn-sm:first').addClass('active');
-    });
-    </script>
     </div>
     <!--  -->
     <div class="contentcos2" id="5000">
@@ -325,31 +243,9 @@ $(document).ready(function(){
                   </li>
                 </ul>
                 <div class="form-group">
-                      <div id="compose-textarea" class="form-control" contentEditable style="height: 300px; overflow-y: scroll;">
-                        <h1><u>Heading Of Message</u></h1>
-                        <h4>Subheading</h4>
-                        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain
-                          was born and I will give you a complete account of the system, and expound the actual teachings
-                          of the great explorer of the truth, the master-builder of human happiness. No one rejects,
-                          dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know
-                          how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again
-                          is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain,
-                          but because occasionally circumstances occur in which toil and pain can procure him some great
-                          pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise,
-                          except to obtain some advantage from it? But who has any right to find fault with a man who
-                          chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that
-                          produces no resultant pleasure? On the other hand, we denounce with righteous indignation and
-                          dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so
-                          blinded by desire, that they cannot foresee</p>
-                        <ul>
-                          <li>List item one</li>
-                          <li>List item two</li>
-                          <li>List item three</li>
-                          <li>List item four</li>
-                        </ul>
-                        <p>Thank you,</p>
-                        <p>John Doe</p>
-                      </div>
+                  <div id="compose-textarea" class="form-control" contentEditable style="height: 300px; overflow-y: scroll;">
+
+                  </div>
                 </div>
                 <div class="box-footer">
                   <div class="pull-right">
@@ -504,9 +400,78 @@ $(document).ready(function(){
     });
     </script>
   </div>
-        <div id="5003" class="row" style="position:relative;top:-1000px;left: 40px;z-index: 1;">
-          <div class="col-lg-9" style="width:100%;">
-              <div class="table-responsive table--no-card m-b-30"style="overflow-x: hidden; width:600px">
+  <div class="contentcos4" id="5005">
+    <section class="content" style="width:1000px;">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Read Mail</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body no-padding">
+              <div class="mailbox-read-info">
+                <h3>Message Subject Is Placed Here</h3>
+                <h5>From: help@example.com
+                  <span class="mailbox-read-time pull-right">15 Feb. 2016 11:03 PM</span></h5>
+              </div>
+              <!-- /.mailbox-read-info -->
+              <!-- /.mailbox-controls -->
+              <div class="mailbox-read-message">
+                <p>Hello John,</p>
+
+                <p>Keffiyeh blog actually fashion axe vegan, irony biodiesel. Cold-pressed hoodie chillwave put a bird
+                  on it aesthetic, bitters brunch meggings vegan iPhone. Dreamcatcher vegan scenester mlkshk. Ethical
+                  master cleanse Bushwick, occupy Thundercats banjo cliche ennui farm-to-table mlkshk fanny pack
+                  gluten-free. Marfa butcher vegan quinoa, bicycle rights disrupt tofu scenester chillwave 3 wolf moon
+                  asymmetrical taxidermy pour-over. Quinoa tote bag fashion axe, Godard disrupt migas church-key tofu
+                  blog locavore. Thundercats cronut polaroid Neutra tousled, meh food truck selfies narwhal American
+                  Apparel.</p>
+
+                <p>Raw denim McSweeney's bicycle rights, iPhone trust fund quinoa Neutra VHS kale chips vegan PBR&amp;B
+                  literally Thundercats +1. Forage tilde four dollar toast, banjo health goth paleo butcher. Four dollar
+                  toast Brooklyn pour-over American Apparel sustainable, lumbersexual listicle gluten-free health goth
+                  umami hoodie. Synth Echo Park bicycle rights DIY farm-to-table, retro kogi sriracha dreamcatcher PBR&amp;B
+                  flannel hashtag irony Wes Anderson. Lumbersexual Williamsburg Helvetica next level. Cold-pressed
+                  slow-carb pop-up normcore Thundercats Portland, cardigan literally meditation lumbersexual crucifix.
+                  Wayfarers raw denim paleo Bushwick, keytar Helvetica scenester keffiyeh 8-bit irony mumblecore
+                  whatever viral Truffaut.</p>
+
+                <p>Post-ironic shabby chic VHS, Marfa keytar flannel lomo try-hard keffiyeh cray. Actually fap fanny
+                  pack yr artisan trust fund. High Life dreamcatcher church-key gentrify. Tumblr stumptown four dollar
+                  toast vinyl, cold-pressed try-hard blog authentic keffiyeh Helvetica lo-fi tilde Intelligentsia. Lomo
+                  locavore salvia bespoke, twee fixie paleo cliche brunch Schlitz blog McSweeney's messenger bag swag
+                  slow-carb. Odd Future photo booth pork belly, you probably haven't heard of them actually tofu ennui
+                  keffiyeh lo-fi Truffaut health goth. Narwhal sustainable retro disrupt.</p>
+
+                <p>Skateboard artisan letterpress before they sold out High Life messenger bag. Bitters chambray
+                  leggings listicle, drinking vinegar chillwave synth. Fanny pack hoodie American Apparel twee. American
+                  Apparel PBR listicle, salvia aesthetic occupy sustainable Neutra kogi. Organic synth Tumblr viral
+                  plaid, shabby chic single-origin coffee Etsy 3 wolf moon slow-carb Schlitz roof party tousled squid
+                  vinyl. Readymade next level literally trust fund. Distillery master cleanse migas, Vice sriracha
+                  flannel chambray chia cronut.</p>
+
+                <p>Thanks,<br>Jane</p>
+              </div>
+            </div>
+            <div class="box-footer">
+              <div class="pull-right">
+                <button type="button" class="btn btn-default"><i class="fa fa-reply"></i> Voltar para Tras</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </section>
+  </div>
+  <script>
+    function entrar(e) {
+      alert(e);
+    }
+  </script>
+    <div id="5003" class="row" style="position:relative;top:-1860px;left: 40px;z-index: 1;">
+        <div class="col-lg-9" style="width:100%;">
+            <div class="table-responsive table--no-card m-b-30"style="overflow-x: hidden; width:600px">
                   <table class="table table-borderless table-striped table-earning">
                       <thead style="display:block;">
                           <tr>
